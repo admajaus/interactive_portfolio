f2dc91bb28fa3cc0f277be949d7145b6db7849c9
admajaus <149647019+admajaus@users.noreply.github.com>
2024-02-09 17:49:46 -0500

    Update jotform_iframe.py

--- contact_about_me/jotform_iframe.py
+++ contact_about_me/jotform_iframe.py
@@ -2,7 +2,7 @@ from dash import html
 
 # Embded contact form from Jotform
 contact_form_iframe = html.Iframe(
-    src ='https://form.jotform.com/240384965371160',
+    src ='<INSERT_UNIQUE_JOTFORM_URL>',
     width = '100%',
     height = '600px',
 )
